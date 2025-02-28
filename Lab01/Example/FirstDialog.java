59729a0ff955dbb96b1e2041969bf85b3eccbfc8
Beanitto <minhdo.sec@gmail.com>
2025-02-28 08:47:18 +0700

    Second example

--- Lab01/Example/FirstDialog.java
+++ Lab01/Example/FirstDialog.java
@@ -1,6 +1,6 @@
 package Lab01.Example;
-
 import javax.swing.JOptionPane;
+
 public class FirstDialog {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hello world! How are you?");
